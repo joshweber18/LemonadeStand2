e714c073b7554c6fd3008483f7f2dd8ad2aa940b
joshweber18 <josh18weber@gmail.com>
2018-10-31 10:41:57 -0500

    Made Customer decision function.

--- LemonadeStand/LemonadeStand/Customer.cs
+++ LemonadeStand/LemonadeStand/Customer.cs
@@ -12,12 +12,40 @@ namespace LemonadeStand
 
 
         //constructor
+        Weather weather = new Weather();
 
         //member methods
-        public virtual void BuyLemonade()
+        public void ChoiceToBuy()
         {
-
+            if (weather.weatherOptions[0] == weather.ChooseWeather())
+            {
+                int x = RandomNumberCustomer() - 10;
+            }
+            if (weather.weatherOptions[1] == weather.ChooseWeather())
+            {
+                int x = RandomNumberCustomer() + 5;
+            }
+            if (weather.weatherOptions[2] == weather.ChooseWeather())
+            {
+                int x = RandomNumberCustomer() + 10;
+            }
+            if (weather.weatherOptions[3] == weather.ChooseWeather())
+            {
+                int x = RandomNumberCustomer() - 5;
+            }
         }
+
+        public int RandomNumberCustomer()
+        {
+            Random random = new Random();
+            int randomCustomerNumber = random.Next(50, 70);
+            return randomCustomerNumber;
+
+
+            
+        }
+
+       
 
     }
     
